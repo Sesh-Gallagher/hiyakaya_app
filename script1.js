6ca1388bf98a49fc5be7b4a2a3efcc2206f8cce0
root <t.ntwana@gmail.com>
2024-09-17 20:09:05 -0700

    projecr

---
+++ script1.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const getStartedBtn = document.getElementById('get-started-btn');
+    if (getStartedBtn) {
+        getStartedBtn.addEventListener('click', function() {
+            window.location.href = 'C:/Users/user/Documents/HiyaKaya Train ticket booking app/index.html';
+        });
+    }
+});
