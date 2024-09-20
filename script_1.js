3d33ed0970d5ab095810db52ac0c41d25f51cdda
root <t.ntwana@gmail.com>
2024-09-20 08:31:49 -0700

    fix

--- script_1.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const getStartedBtn = document.getElementById('get-started-btn');
-    if (getStartedBtn) {
-        getStartedBtn.addEventListener('click', function() {
-            window.location.href = 'C:/Users/user/Documents/HiyaKaya Train ticket booking app/index.html';
-        });
-    }
-});
